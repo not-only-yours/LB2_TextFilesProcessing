b5454bc7b08f76071520f8ca6fd8baa367914434
Yaroslaww1 <yaroslaww1@gmail.com>
2020-02-19 20:44:52 +0200

    added Student class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+
 public class Main {
 
+    public static void main(String[] args) {
 
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(50);
+        list.add(1);
+
+        Student student = new Student("kappa", list, false);
+        System.out.println(student.getAverageRating());
+    }
 
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
+
 public class Student {
+
+    private final String name;
+    private final ArrayList<Integer> rating;
+    private final Boolean budget;
+
+    public Student(String name, ArrayList<Integer> rating, Boolean budget) {
+        this.name = name;
+        this.rating = rating;
+        this.budget = budget;
+    }
+
+    public Double getAverageRating() {
+
+        Integer summaryRating = 0;
+
+        for (Integer rate : rating) {
+            summaryRating += rate;
+        }
+
+        Double averageRating = (double)(summaryRating) / (double)rating.size();
+
+        return averageRating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
