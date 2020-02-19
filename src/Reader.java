a771e8dd0770393c87759b19afdab0ad234a0e80
Nikita Bakun <not-only-yours@users.noreply.github.com>
2020-02-19 21:23:13 +0200

    neww

--- src/Reader.java
+++ src/Reader.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class Reader {
+    public static void read(String[] args) throws FileNotFoundException {
+        Scanner reader = new Scanner(new File("Import.txt"));
+       int numberOfString = reader.nextInt();
+        Object[] Students = new Object[numberOfString];
+
+
+        while(reader.hasNext()) {
+            String boof = reader.nextLine();
+
+
+        }
+    }
 }
+
+
+class Students{
+    String name;
+    int rating;
+    boolean budget;
+        }
