c029b9fa357f89eb63cdf16e9621153bb44574b4
Nikita Bakun <not-only-yours@users.noreply.github.com>
2020-02-20 09:06:36 +0200

    output

--- src/utputToCsv.java
+++ src/utputToCsv.java
@@ -1,23 +1,21 @@
 import java.io.IOException;
 import java.io.*;
 import java.util.ArrayList;
-
+import java.io.IOException;
 class OutputToCsv {
-    public void Output(ArrayList<Student> studentsList) {
-        try (FileWriter writer = new FileWriter("Output.csv")) {
-            writer.write(studentsList.Student);
-            writer.append('\n');
-            writer.flush();
-        } catch (IOException ex) {
-            System.out.println((ex.getMessage()));
+    public void Output(ArrayList<Student> studentsList,String filename) {
+        try (FileWriter writer = new FileWriter(filename)) {
+            for (Student student : studentsList) {
+                writer.write(student.getName() + ",") ;
+                writer.write(String.valueOf(student.getBudget()));
+                writer.append('\n');
+                writer.flush();
+            }
+        }
+             catch(IOException ex){
+                System.out.println((ex.getMessage()));
+            }
         }
     }
-    public void Input() throws FileNotFoundException{
-        FileReader reader = new FileReader("Input.csv");
-        String bool;
-        while(reader.hasNext()){
-            
-        }
-    }
-}
+
 
