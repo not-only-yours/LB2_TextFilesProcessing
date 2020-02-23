12b20d7800f41ca1750a5458cdeb3de5238ae2b1
Nikita Bakun <not-only-yours@users.noreply.github.com>
2020-02-23 22:19:34 +0200

    Input

--- src/MyReader.java
+++ src/MyReader.java
@@ -3,31 +3,38 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class MyReader {
-
-    public MyReader() {
-
-    }
+    int numberOfString;
+    boolean budgetOfStudent;
+    String[] nameOfStudent;
+    String nameStudent;
+    ArrayList<Integer> markOfStudent;
+    String buff;
+    String[] buffToInt;
+    String toInt;
 
     public ArrayList<Student> read(String filename) throws FileNotFoundException {
         Scanner scanner = new Scanner(new File(filename));
-
-        int numberOfString = scanner.nextInt();
-
+        numberOfString = scanner.nextInt();
         System.out.println(numberOfString);
-       String nameOfStudent = new String();
+        nameOfStudent = new String[50];
         ArrayList<Student> students = new ArrayList<>();
-        String buff = new String();
+        buff = new String();
         while (scanner.hasNextLine()) {
             buff = scanner.nextLine();
-            nameOfStudent = new buff.split(",");
-            ArrayList<Integer> markOfStudent = new ArrayList<>();
-            markOfStudent.add(Integer.parseInt(new buff.split(",")));
-            boolean budgetOfStudent;
+            nameOfStudent = buff.split(",");
+            markOfStudent = new ArrayList<>();
+            buffToInt = new String[50];
+            buffToInt=buff.split(",");
+            toInt=new String();
+            toInt= buffToInt.toString();
+            markOfStudent.add(Integer.parseInt(toInt));
             String[] isBudget;
             isBudget = buff.split(",");
             if (isBudget.equals("TRUE")) budgetOfStudent = true;
             else budgetOfStudent = false;
-            Student student = new Student(nameOfStudent, markOfStudent, budgetOfStudent);
+            nameStudent = new String();
+            nameStudent=nameOfStudent.toString();
+            Student student = new Student(nameStudent, markOfStudent, budgetOfStudent);
             students.add(student);
         }
         return students;
