bbbfc2e73a78265249c0fdfafc32ed4a1954c7dc
Yaroslaww88 <yaroslaww1@gmail.com>
2020-02-27 09:07:13 +0200

    add option to choose dir

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,21 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<Integer> list1 = new ArrayList<>();
-        list1.add(50);
-        list1.add(1);
-        Student student1= new Student("kappa1", list1, true);
-
-        ArrayList<Integer> list2 = new ArrayList<>();
-        list2.add(1000);
-        Student student2 = new Student("kappa2", list2, false);
-
-        ArrayList<Integer> list3 = new ArrayList<>();
-        list3.add(1);
-        Student student3 = new Student("kappa3", list3, false);
-
         OutputToCsv output = new OutputToCsv();
-        File[] files =  output.filesInFolder("./input");
+
+        System.out.println("Enter directory: ");
+        Scanner scanner = new Scanner(System.in);
+        String dir = scanner.nextLine();
+
+        File[] files =  output.filesInFolder(dir);
 
         RatingTable ratingTable = new RatingTable();
-        /*ratingTable.addStudent(student1);
-        ratingTable.addStudent(student2);
-        ratingTable.addStudent(student3);*/
 
         for (File file : files) {
             System.out.println(file);
